4e3c48d1aceda68c97da65db2f3f378c51cb4363
datbeohbbh <phongtomfapp@gmail.com>
2020-01-21 00:35:10 +0300

    template for cp

---
+++ main.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        InputStream inputStream;
+        OutputStream outputStream;
+        if(System.getProperty("ONLINE_JUDGE") == null) {
+            File input = new File("test.inp");
+            inputStream = new FileInputStream(input);
+        }
+        else {
+            inputStream = System.in;
+        }
+        outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class Task {
+
+        public void solve(InputReader in, PrintWriter out) {
+            
+        }
+
+    }
+    
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
+
+
---
+++ modular.cpp
@@ -0,0 +1,97 @@
+template <class T>
+struct Modular{
+    const T base = (T)(1e9 + 7);
+    T val;
+    Modular(){
+        val = 0;
+    }
+    Modular(const T &new_val){
+        val = new_val % base;
+        val = (val >= base ? val - base : (val < 0 ? val + base : val));
+    }
+    void operator = (const Modular <T> &x){
+        val = x.val % base;
+    }
+    friend bool operator == (const Modular <T> &x,const Modular <T> &y){
+        return x.val == y.val;
+    }
+    friend bool operator != (const Modular <T> &x,const Modular <T> &y){
+        return !(x == y);
+    }
+    friend bool operator < (const Modular <T> &x,const Modular <T> &y){
+        return x.val < y.val;
+    }
+    friend bool operator > (const Modular <T> &x,const Modular <T> &y){
+        return x.val > y.val;
+    }
+    friend bool operator <= (const Modular <T> &x,const Modular <T> &y){
+        return (x < y) or (x == y);
+    }
+    friend bool operator >= (const Modular <T> &x,const Modular <T> &y){
+        return (x > y) or (x == y);
+    }
+    friend ostream& operator << (ostream &out,const Modular <T> &x){
+        return out << x.val;
+    }
+    friend istream& operator >> (istream &in,Modular <T> &x){
+        return in >> x.val;
+    }
+    Modular <T> operator += (const Modular <T> &x){
+        val = (val % base + x.val % base);
+        val -= (val > base ? base : 0);
+        return *this;
+    }
+    Modular <T> operator -= (const Modular <T> &x){
+        val =(val % base - x.val % base);
+        val += (val < 0 ? base : 0);
+        return *this;
+    }
+    Modular <T> operator + (const Modular <T> &x){
+        return (*this += x);
+    }
+    Modular <T> operator - (const Modular <T> &x){
+        return (*this -= x);
+    }
+    Modular <T> operator - () const {
+        return Modular <T> (0) - *this;
+    }
+    Modular <T> operator ++ (){
+        return (*this += Modular <T> (1));
+    }
+    Modular <T> operator -- (){
+        return (*this -= Modular <T> (1));
+    }
+    Modular <T> operator *= (const Modular <T> &x){
+        val = ((val % base) * (x.val % base)) % base;
+        return *this;
+    }
+    Modular <T> operator * (const Modular <T> &x){
+        return (*this *= x);
+    }
+    Modular <T> operator ^= (T h){
+        Modular <T> res = Modular <T> (1);
+        Modular <T> x = *this;
+        for(;h > 0;x =x * x,h /= (T)2){
+            if(h & 1){
+                res *= x;
+            }
+        }
+        *this = res;
+        return res;
+    }
+    Modular <T> operator ^ (T h){
+        return (*this ^= h);
+    }
+    Modular <T> operator ^= (Modular <T> h){
+        return (*this ^= h.val);
+    }
+    Modular <T> operator ^ (Modular <T> h){
+        return (*this ^= h.val);
+    }
+    Modular <T> operator /= (Modular <T> &x){
+        return (*this *= (x ^ (base - 2)));
+    }
+    Modular <T> operator / (Modular <T> &x){
+        return (*this /= x);
+    }
+};
